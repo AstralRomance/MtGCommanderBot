4d92d95019a0c2d34535ecefab72c61a00f82b47
AstralRomance <andrdoncwork@mail.ru>
2021-05-10 23:15:26 +0300

    Some tests added
    Add some tests.

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -27,3 +27,10 @@ def test_correct_response_for_full_name(card_name):
 def test_non_empty_parser_response(card_name):
     response = requests.post(PARSER_URL, data=json.dumps(make_parser_header(card_scg_search_form(card_name))))
     assert response.json()['items'] is not None
+
+@pytest.mark.parametrize('card_info', [{'Thief of Sanity': ['$1.99', 'Guilds of Ravnica']}, {'Supreme Verdict': ['$7.99', 'Iconic Masters']}])
+def test_correct_price(card_info):
+    set_val = list(card_info.items())[0][1][1]
+    response = requests.post(PARSER_URL, data = json.dumps(make_parser_header(card_scg_search_form(str(list(card_info.items())[0][0])))))
+    assert response.json()['items'][0][set_val][0]['Near Mint'] == list(card_info.items())[0][1][0]
+    
--- tests/scryfall_api_test.py
+++ tests/scryfall_api_test.py
@@ -2,8 +2,17 @@ import pytest
 import requests
 from constants import SCRYFALL_API_URL
 
-@pytest.mark.parametrize('card_name', [{'вор рассудка':'Thief of Sanity'}, {'Унижение': 'Mortify'}, {'ТеФеРи, ГеРоЙ': 'Teferi, Hero of Dominaria'}])
+@pytest.mark.parametrize('card_name', [{'test_name': 'Вор рассудка', 'expected_name': 'thief of sanity'}, {'test_name': 'Mortify', 'expected_name': 'mortify'}])
+def test_regular_search(card_name):
+    response = requests.get(''.join((SCRYFALL_API_URL, f'/cards/named?fuzzy={card_name["test_name"].replace(" ", "+")}')))
+    assert response.json()['name'].lower() == card_name['expected_name']
+
+@pytest.mark.parametrize('card_name', [{'вор рассудка':'Thief of Sanity'}, {'Унижение': 'Mortify'}, {'ТеФеРи, ГеРоЙ ДоМиНаРиИ': 'Teferi, Hero of Dominaria'}])
 def test_rus_card_scryfall_api_response(card_name):
     response = requests.get(''.join((SCRYFALL_API_URL, f'/cards/named?fuzzy={list(*card_name.items())[0].replace(" ", "+")}')))
-    print(response.json()['name'])
-    assert response.json()['name'] == list(*card_name.items())[1]
+    assert response.json()['name'].lower() == list(*card_name.items())[1].lower()
+
+@pytest.mark.parametrize('card_name', [{'test_name': 'Вор расс', 'expected_name': 'thief of sanity'}, {'test_name': 'тефери, геро', 'expected_name': 'teferi, hero of dominaria'}])
+def test_part_name(card_name):
+    response = requests.get(''.join((SCRYFALL_API_URL, f'/cards/named?fuzzy={card_name["test_name"].replace(" ", "+")}')))
+    assert response.json()['name'].lower() == card_name['expected_name']
