0e4cc605a77de91ecb4c635fcffc4bc04706d641
AstralRomance <andrdoncwork@mail.ru>
2021-12-12 19:59:37 +0300

    Add dockerfiles and another deployment stuff

--- tg_bot/URL_CONSTANTS.py
+++ tg_bot/URL_CONSTANTS.py
@@ -1,4 +1,6 @@
+import os
+
 SCRYFALL_API_URL = 'https://api.scryfall.com'
 STARCITY_SEARCH = r'https://starcitygames.hawksearch.com/sites/starcitygames/?card_name='
 STARCITY_LINK = r'https://starcitygames.com/search/?search_query='
-PARSER_URL = 'http://localhost:9080/crawl.json'
+PARSER_URL = f'http://{os.environ.get("SCRAPY_CONTAINER")}:{os.environ.get("SCRAPY_PORT")}/crawl.json'
